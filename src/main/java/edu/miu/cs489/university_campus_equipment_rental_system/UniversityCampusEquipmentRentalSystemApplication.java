4e1ef9ce4c5b05877e823e1ef24c0b4d12d4b755
chinawutao <462755350@qq.com>
2023-10-27 00:20:09 -0500

    first commit

---
+++ src/main/java/edu/miu/cs489/university_campus_equipment_rental_system/UniversityCampusEquipmentRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.university_campus_equipment_rental_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCampusEquipmentRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityCampusEquipmentRentalSystemApplication.class, args);
+	}
+
+}
